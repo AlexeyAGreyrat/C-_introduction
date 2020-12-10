34f5e7fbec61deb0ef464e86b255b71610fcdc54
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2020-12-10 12:18:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lesson1_lab1
+{
+    class Program
+    {       
+        static void Main()
+        {
+            Console.WriteLine("Введите имя пользвоателя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Привет," + name + " сегодня " + DateTime.Now);            
+        }
+    }
+}
