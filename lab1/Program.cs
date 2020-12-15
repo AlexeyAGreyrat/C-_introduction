a3127f9c42158536c71abe6bea882fa1bd0b36a1
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2020-12-15 12:46:55 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+            #region Задание 1
+            Console.WriteLine("Ввдите  максимальную температуру за сутки");
+            int temMax = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввдите минимальную температуру за сутки");
+            int temMin = Convert.ToInt32(Console.ReadLine());
+            float temS = (float)(temMax + temMin) / 2;
+            Console.WriteLine($"Средняя температура сегодня {temS}");
+            #endregion 
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        public enum Month
+        {
+            Январь  = 1,
+            Февраль,
+            Март, 
+            Апрель,
+            Май,
+            Июнь, 
+            Июль, 
+            Август, 
+            Сетнябрь, 
+            Октябрь, 
+            Ноябрь, 
+            Декабрь,
+        }
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+            #region Задание 2
+            Console.WriteLine("Введите порядковый номер текущего месяца");
+            int month = Convert.ToInt32(Console.ReadLine());
+            if(month <= 12 && month > 0 )
+            {
+                Month month1 = (Month)month;
+                Console.WriteLine(month1);
+            }
+            else            
+                Console.WriteLine("Ошибка порядковый номер месеца не существует");
+            #endregion
+        }
+    }
+}
