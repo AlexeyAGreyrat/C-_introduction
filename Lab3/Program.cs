519af5b00d494c197f1ca931b7581c4be3cfe93c
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2020-12-18 18:05:19 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите строку");
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[(str.Length-1) - i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
