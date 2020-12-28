63249f67cfeb32224495fa498b18ba9a314f885b
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2020-12-28 07:40:32 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] s = null;
+            while (true)
+            {
+                bool Schek = true;                
+                Console.WriteLine("Введите последовательность");
+                string str = Console.ReadLine();                
+                s = new byte[str.Split(' ').Length];
+                for (int i = 0; i < str.Split(' ').Length; i++)
+                {
+                    if (Convert.ToInt32(str.Split(' ')[i]) < 255 & Convert.ToInt32(str.Split(' ')[i]) > 0)
+                        s[i] = Convert.ToByte(str.Split(' ')[i]);
+                    else
+                    {
+                        Console.WriteLine($"Ошибка Введите Числа от 0 до 255");
+                        Schek = false;
+                        break;
+                    }
+                }                
+                if(Schek == true)
+                {
+                    break;
+                }
+            }
+            File.WriteAllBytes("bytes.bin", s);           
+        }
+    }
+}
---
+++ Lab5/ToDo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lab5
+{
+    [Serializable]
+    public class ToDo
+    {
+        public string[] Title { get; set; }
+        public bool[] IsDone { get; set; }
+        public ToDo(string[] Title, bool[] IsDone)
+        {
+            this.Title = Title;
+            this.IsDone = IsDone;
+        }
+        public ToDo()
+        {
+            this.Title = null;
+            this.IsDone = null;
+        }
+
+    }
+}
